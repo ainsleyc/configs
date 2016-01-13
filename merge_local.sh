63f17a6b6591767e1276fde25c4d9c4872d4720f
Ainsley Chong <ainsley.chong@gmail.com>
2016-01-13 13:32:27 -0800

    Added tmux merge

--- merge_local.sh
+++ merge_local.sh
@@ -7,15 +7,19 @@
 # The following files are merged:
 #   ./.bashrc + ~/.bashrc_local 
 #   ./.vimrc + ~/.vimrc_local 
+#   ./.tmux.conf + ~/.tmux_local.conf
 ##################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-BASHRC_PATH="${HOME}/.bashrc_test"
+BASHRC_PATH="${HOME}/.bashrc"
 BASHRC_LOCAL_PATH="${HOME}/.bashrc_local"
 
-VIMRC_PATH="${HOME}/.vimrc_test"
+VIMRC_PATH="${HOME}/.vimrc"
 VIMRC_LOCAL_PATH="${HOME}/.vimrc_local"
+
+TMUX_PATH="${HOME}/.tmux.conf"
+TMUX_LOCAL_PATH="${HOME}/.vimrc_local.conf"
 
 # .bashrc
 printf "\n### Github ###\n" > ${BASHRC_PATH}
@@ -29,6 +33,15 @@ fi
 # .vimrc
 printf "\n### Github ###\n" > ${VIMRC_PATH}
 cat ${DIR}/.vimrc >> ${VIMRC_PATH}
+printf "\n### Local ###\n" >> ${VIMRC_PATH}
+if [ -e ${VIMRC_LOCAL_PATH} ] 
+then
+  cat ${VIMRC_LOCAL_PATH} >> ${VIMRC_PATH}
+fi
+
+# .tmux.conf
+printf "\n### Github ###\n" > ${VIMRC_PATH}
+cat ${DIR}/.tmux.conf >> ${VIMRC_PATH}
 printf "\n### Local ###\n" >> ${VIMRC_PATH}
 if [ -e ${VIMRC_LOCAL_PATH} ] 
 then
